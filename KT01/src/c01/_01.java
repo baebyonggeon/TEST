48ddb6f9a27a7bcb7ecc0b0742142267edaaaaea
byonggeon <bk.bae@gnewsoft.com>
2024-01-12 17:24:03 +0900

    test

---
+++ KT01/src/c01/_01.java
@@ -0,0 +1,12 @@
+package c01;
+
+public class _01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		
+
+	}
+
+}
---
+++ KT01/src/c01/_02_EC.java
@@ -0,0 +1,10 @@
+package c01;
+
+public class _02_EC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("write");
+	}
+
+}
